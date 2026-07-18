07e6c909035c0ef99550409edb560a0c601c16d1
Suman Chandra Barman <sumanchandrabarman999@gmail.com>
2026-07-18 15:07:18 +0600

    chore: update image remote patterns and add favicon to dashboard

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.devtunnels.ms",
-      }
+        hostname: "**.weddingworld.de",
+      },
     ],
   },
 };
